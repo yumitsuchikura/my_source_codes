01a44120a20464e7faf3f764faf94beb314dfddc
yumit <yutsu0216@yahoo.co.jp>
2021-11-17 21:04:55 +0900

    1. WEB・AP・DB停止・DBバックアップのシェルスクリプト追加 2. 資源コピーマクロの別バージョン追加

---
+++ ShellScript/00001_WEB・AP・DB停止・DBバックアップ/before_shutdown.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# EC2: kakeibo-web (35.75.156.133)
+# 格納パス: /home/ec2-user/sh/before_shutdown.sh
+
+# WEBサーバ停止
+sudo systemctl stop httpd.service
+
+# APサーバ停止
+sudo systemctl stop tomcat.service
+
+# MariaDB接続設定
+DBHOST="127.0.0.1"
+DBUSER="tsuchi"
+DBPASS="tsuchi"
+DBNAME="tsuchi_kakeibo"
+
+# DBバックアップ設定
+BACKUP_DIR="/tmp/db_backup"
+BACKUP_ROTATE=3
+MYSQLDUMP="/usr/bin/mysqldump"
+
+# バックアップ出力先ディレクトリのチェック
+if [ ! -d "$BACKUP_DIR" ]; then
+        echo "バックアップ出力先ディレクトリが存在しません: $BACKUP_DIR" >&2
+        exit 1
+fi
+
+# 今日の日付をYYYYMMDDで取得
+today=$(date '+%Y%m%d')
+
+# mysqldumpコマンドでDBのバックアップを取得
+$MYSQLDUMP -h "${DBHOST}" -u "${DBUSER}" -p"${DBPASS}" "${DBNAME}" > "${BACKUP_DIR}/${DBNAME}-${today}.dump"
+
+# mysqldumpコマンドの終了ステータスで成功・失敗を確認
+if [ $? -eq 0 ]; then
+        gzip "${BACKUP_DIR}/${DBNAME}-${today}.dump"
+
+        # 古いバックアップファイルを削除する
+        find "$BACKUP_DIR" -name "${DBNAME}-*.dump.gz" -mtime +${BACKUP_ROTATE} | xargs rm -f
+else
+        echo "バックアップ作成失敗: ${BACKUP_DIR}/${DBNAME}-${today}.dump"
+        exit 2
+fi
+
+# DBサーバ停止
+sudo systemctl stop mariadb
